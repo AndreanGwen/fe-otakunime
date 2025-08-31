a49c4ffaa2ead79b37dae370affedd17fac3b4b0
AndreanGwen <andreansatriyagwen@gmail.com>
2025-08-31 18:34:07 +0700

    add fitur baru

--- src/pages/episode/[slug].tsx
+++ src/pages/episode/[slug].tsx
@@ -1,9 +1,12 @@
 import CompleteAnime from "@/Comp/Commons/CompleteAnime";
 import SideBar from "@/Comp/Commons/SideBar";
 import { DarkModeContext } from "@/context/darkModeContext/darkModeContext";
+import Skeleton from "@mui/material/Skeleton";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
+import { FaCalendarAlt, FaUser } from "react-icons/fa";
+import { IoIosArrowBack } from "react-icons/io";
 
 const VideoPage = () => {
   const darkModeContext = useContext(DarkModeContext);
@@ -18,12 +21,26 @@ const VideoPage = () => {
   const { isDarkMode, setIsDarkMode } = darkModeContext;
 
   const [data, setData] = useState<any>([]);
+  const [dataSecond, setDataSecond] = useState<any>([]);
 
   useEffect(() => {
+    if (!slug) return;
     axios.get(`${urlSamehada}/episode/${slug}`).then((res) => {
-      console.log(res.data[0]);
+      setData(res?.data[0]);
     });
-  });
+  }, [slug]);
+
+  useEffect(() => {
+    if (!slug) return;
+    axios
+      .get(`${urlSamehada}/anime/${slug}`)
+      .then((res) => {
+        setDataSecond(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, [slug]);
+
+  console.log(slug);
 
   return (
     <div className="w-full flex min-h-screen">
@@ -38,7 +55,63 @@ const VideoPage = () => {
         className={`w-full ${
           isDarkMode ? "bg-white" : "bg-black"
         } min-h-screen`}
-      ></div>
+      >
+        {data.length === 0 ? (
+          <div className={`pt-9 w-full`}>
+            <Skeleton className="h-[70px] w-[600px]" />
+            <div className={"flex gap-10"}>
+              <Skeleton className="h-[70px] w-[120px]" />
+              <Skeleton className="h-[70px] w-[170px]" />
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className={`flex gap-7 items-start`}>
+              <div className={`pt-10.5 `}>
+                <IoIosArrowBack
+                  size={25}
+                  onClick={() => router.back()}
+                  className="cursor-pointer bg-red-200 flex items-center justify-center rounded-full p-1"
+                />
+              </div>
+              <div className={`flex flex-col w-full`}>
+                <div
+                  className={`pt-9 text-2xl font-semibold pb-3  gap-10 ${
+                    isDarkMode ? "text-black" : "text-white"
+                  }`}
+                >
+                  <h1>{data?.title}</h1>
+                </div>
+
+                <div
+                  className={`flex gap-10 pb-7 ${
+                    isDarkMode ? "text-black" : "text-white"
+                  }`}
+                >
+                  <div className={"flex items-center-safe gap-2"}>
+                    <FaUser color={`${isDarkMode ? "#000000" : "#ffffff"}`} />
+                    <p>{data?.dataPostReleased?.[0].posted}</p>
+                  </div>
+                  <div className={"flex items-center-safe gap-2"}>
+                    <FaCalendarAlt
+                      color={`${isDarkMode ? "#000000" : "#ffffff"}`}
+                    />
+                    <p>{data?.dataPostReleased?.[1].posted}</p>
+                  </div>
+                </div>
+
+                <div className={``}>
+                  <iframe
+                    src={data?.urlFrame}
+                    className={`w-5/6 h-[450px] rounded-lg border-2 border-white`}
+                    allowFullScreen={true}
+                  />
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
 
       <div
         className={`w-3/8 min-h-screen ${
